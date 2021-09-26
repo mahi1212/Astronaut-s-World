d717880438bc3f6c023257d0a71b9030e82047ca
mahi1212 <mahi121.mr@gmail.com>
2021-09-26 07:47:45 +0600

    initial commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <h2>The Astronauts Special Branch</h2>
+            <h3>Everything is possible! Time to live in another <span>PLANET</span></h3>
+            <h2>TOTAL BUDGET : 100 Billion US $</h2>
+        </div>
+    );
+};
+
+export default Header;
